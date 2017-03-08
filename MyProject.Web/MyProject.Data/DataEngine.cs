dc5371d55fc614e0eb93be2c34ced1bfc8d1fb91
Sephiroth1550 <Sephiroth1550@gmail.com>
2017-03-08 10:14:38 -0600

    Updated after the demonstration yesterday

---
+++ MyProject.Web/MyProject.Data/DataEngine.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+
+namespace MyProject.Data
+{
+    public static class DataEngine
+    {
+        public static Account GetAccount(int AccountID)
+        {
+            Account returnAccount = new Account();
+
+            using (var db = new TestDatabaseEntities())
+            {
+
+                List<Account> Accounts = db.Accounts.Where(a => a.AccountID == AccountID).Distinct().ToList();
+
+                foreach (var Account in Accounts)
+                {
+
+                    returnAccount.Email = Account.Email;
+                    returnAccount.Password_ = Account.Password_;
+
+
+                }
+            
+            
+            
+            }
+
+
+            return returnAccount;
+
+
+        }
+
+
+
+    }
+}
